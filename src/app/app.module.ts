0e97a4af6690207878ddd6b9a2495ca036f7cd97
Shruti Priya <shrutipriya1808@gmail.com>
2017-04-07 08:18:34 -0700

    add component welcome, about and member

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { MemberComponent } from './member/member.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    AboutComponent,
+    MemberComponent
   ],
   imports: [
     BrowserModule,
