4d45dda2eaaf20834eda79858ca289931fa71fb4
Shruti Priya <shrutipriya1808@gmail.com>
2017-04-07 09:01:54 -0700

    add navbar for routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,7 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent }   from './about/about.component';
 
 const appRoutes: Routes = [
   {
