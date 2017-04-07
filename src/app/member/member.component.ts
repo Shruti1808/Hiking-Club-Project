20232eab71a99edcc05cdae60cc75f4673c95191
Shruti Priya <shrutipriya1808@gmail.com>
2017-04-07 09:44:53 -0700

    displays the list of members on webpage

--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Member } from '../member.model';
 
 @Component({
   selector: 'app-member',
   templateUrl: './member.component.html',
   styleUrls: ['./member.component.css']
 })
-export class MemberComponent implements OnInit {
+export class MemberComponent  {
 
-  constructor() { }
+  members: Member[] = [
+    new Member("Anthony J","https://secure.meetupstatic.com/photos/member/1/b/2/8/member_240006952.jpeg","Lacey,WA","March 9,2017"),
+    new Member("Breezy M","https://secure.meetupstatic.com/photos/member/c/d/7/6/member_265132598.jpeg","Seattle,WA","November 4,2015"),
+    new Member("Brena B","https://secure.meetupstatic.com/photos/member/f/1/7/member_264783863.jpeg","Portland,WA","January 9,2012"),
+    new Member("Christina S","https://secure.meetupstatic.com/photos/member/5/9/6/2/member_234922882.jpeg","Olympia,WA","March 9,2017"),
+    new Member("Derk","https://secure.meetupstatic.com/photos/member/d/d/5/0/member_3176656.jpeg","Tacoma,WA","February 24,2017"),
+    new Member("Donnie","https://secure.meetupstatic.com/photos/member/9/4/1/8/member_201517912.jpeg","Lacey,WA","July 2,2016")
 
-  ngOnInit() {
-  }
+  ]
 
 }
